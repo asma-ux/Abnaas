62302f0f82dbfeead67c79b48c32d03763eb2fc5
najmamoh <mnajma179@gmail.com>
2021-10-14 11:11:24 +0300

    Express update

--- Router/productRouter.js
+++ Router/productRouter.js
@@ -1,9 +1,10 @@
-const express=require('express')
-const productController=require('../controller/productController')
-const Router=express.Router();
-Router.route('/').post(productController.products).get(productController.findproduct)
+const express = require("express");
+const productController = require("../controller/productController");
+const Router = express.Router();
+Router.route("/")
+  .post(productController.products)
+  .get(productController.findproduct);
 
+Router.route("/:id").get(productController.getfind);
 
-
-module.exports=Router
-
+module.exports = Router;
